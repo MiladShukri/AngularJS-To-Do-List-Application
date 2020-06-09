38f0f867fc9acb8240989e5ca6d4f3d71e35ed9b
MiladShukri <50368318+MiladShukri@users.noreply.github.com>
2020-06-09 22:08:08 +0100

    Completed priority states
    when adding a new task placeholder for <select> disappears need to fix this

--- app/app.js
+++ app/app.js
@@ -12,73 +12,98 @@ ToDoListApp.run(function(){
 
 ToDoListApp.controller('ToDoController', ['$scope', function($scope){
 
+  $scope.priorities = ["Top", "Medium", "Low"];
+
   $scope.removeTask = function(task){
     var removedTask = $scope.tasks.indexOf(task);
     $scope.tasks.splice(removedTask,1)
   }
 
   $scope.addTask = function(){
-    $scope.tasks.push({
-      name: $scope.newTask.name,
-      priority: $scope.newTask.priority,
-      duration: parseInt($scope.newTask.duration),
-      available: true
-    });
+    if ($scope.newTask.priority == "Top"){
+      $scope.tasks.push({
+        task: $scope.newTask.task,
+        priority: $scope.newTask.priority,
+        duration: parseInt($scope.newTask.duration),
+        available: true,
+        priorityColour: "Red"
+      });
+    }
+    else if ($scope.newTask.priority == "Medium"){
+      $scope.tasks.push({
+        task: $scope.newTask.task,
+        priority: $scope.newTask.priority,
+        duration: parseInt($scope.newTask.duration),
+        available: true,
+        priorityColour: "Orange"
+      });
+    }
+    else if ($scope.newTask.priority == "Low"){
+      $scope.tasks.push({
+        task: $scope.newTask.task,
+        priority: $scope.newTask.priority,
+        duration: parseInt($scope.newTask.duration),
+        available: true,
+        priorityColour: "Green"
+      });
+    }
 
-    $scope.newTask.name = "";
+
+    $scope.newTask.task = "";
     $scope.newTask.priority = "";
     $scope.newTask.duration = "";
+
   };
 
   $scope.tasks = [
     {
-      name: "Meeting",
-      priority: "Red",
+      task: "Meeting",
+      priorityColour: "Red",
+      priority: "Top",
       duration: 1,
       available: true,
-      thumb: "content/img/icon.png"
     },
     {
-      name: "Workout",
-      priority: "Orange",
+      task: "Workout",
+      priorityColour: "Green",
+      priority: "Low",
       duration: 2,
       available: true,
-      thumb: "content/img/icon.png"
     },
     {
-      name: "Work on some code",
-      priority: "Yellow",
+      task: "Work on some code",
+      priorityColour: "Red",
+      priority: "Top",
       duration: 4,
       available: true,
-      thumb: "content/img/icon.png"
     },
     {
-      name: "Produce some tests for the code",
-      priority: "Green",
+      task: "Produce some tests for the code",
+      priorityColour: "Orange",
+      priority: "Medium",
       duration: 3,
       available: true,
-      thumb: "content/img/icon.png"
     },
     {
-      name: "Upload work to github",
-      priority: "Blue",
+      task: "Upload work to github",
+      priorityColour: "Orange",
+      priority: "Medium",
       duration: 1,
       available: true,
-      thumb: "content/img/icon.png"
     },
     {
-      name: "Grab some shopping",
-      priority: "Indigo",
+      task: "Grab some shopping",
+      priorityColour: "Green",
+      priority: "Low",
       duration: 1,
       available: true,
-      thumb: "content/img/icon.png"
     },
     {
-      name: "Play some games",
-      priority: "Violet",
+      task: "Play some games",
+      priorityColour: "Green",
+      priority: "Low",
       duration: 3,
       available: true,
-      thumb: "content/img/icon.png"
     },
   ];
 
